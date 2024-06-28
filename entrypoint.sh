0c244ffba792f95fc945f9072b68668213d0c7cb
LeuZeuS <104224535+LeuZeuS@users.noreply.github.com>
2024-06-28 01:14:51 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
+# Vérifier si le mot de passe est défini
+if [ -z "$DASHBOARD_PASSWORD" ]; then
+  echo "Erreur : la variable d'environnement DASHBOARD_PASSWORD n'est pas définie."
+  exit 1
+fi
 
 # Vérifier si le dashboard est déjà installé
 if [ ! -f /etc/crowdsec/dashboard_installed ]; then
   echo "Dashboard non installé. Exécution de cscli dashboard setup."
 
   # Exécuter le setup avec les variables d'environnement et le secret
-  cscli dashboard setup --password "$(cat /etc/crowdsec/secrets/password)" \
+  cscli dashboard setup --password "$DASHBOARD_PASSWORD" \
     --listen "${DASHBOARD_LISTEN:-127.0.0.1}" \
     --port "${DASHBOARD_PORT:-3000}"
 
