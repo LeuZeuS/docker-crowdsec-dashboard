3435f4a32e604d50b3b873f7f5a763c1e2593a39
LeuZeuS <104224535+LeuZeuS@users.noreply.github.com>
2024-06-27 22:09:56 -0400

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Vérifier si le dashboard est déjà installé
+if [ ! -f /etc/crowdsec/dashboard_installed ]; then
+  echo "Dashboard non installé. Exécution de cscli dashboard setup."
+
+  # Exécuter le setup avec les variables d'environnement
+  cscli dashboard setup --password "$(cat /etc/crowdsec/secrets/password)" \
+    --listen "${DASHBOARD_LISTEN:-127.0.0.1}" \
+    --port "${DASHBOARD_PORT:-3000}"
+
+  # Marquer le dashboard comme installé
+  touch /etc/crowdsec/dashboard_installed
+else
+  echo "Dashboard déjà installé. Ignorer l'installation."
+fi
+
+# Démarrer le dashboard
+cscli dashboard start
